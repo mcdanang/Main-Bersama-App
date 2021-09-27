91f4a1fbe738077b7a49c808f8ab6ef6f6667fe0
mcdanang <mdanangpriambodo@gmail.com>
2021-09-27 23:42:17 +0700

    ready to deploy

--- config/database.ts
+++ config/database.ts
@@ -50,6 +50,21 @@ const databaseConfig: DatabaseConfig = {
       debug: false,
     },
 
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('MYSQL_HOST'),
+        port: Env.get('MYSQL_PORT'),
+        user: Env.get('MYSQL_USER'),
+        password: Env.get('MYSQL_PASSWORD', ''),
+        database: Env.get('MYSQL_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
   }
 }
 
